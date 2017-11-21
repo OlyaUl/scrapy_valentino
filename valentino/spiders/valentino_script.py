6b928eedcfdaa652dc6da2931a77dc782b82fba2
OlyaOlya <olia-ulyanickaya@mail.ru>
2017-11-21 23:06:28 +0200

    get categories

--- valentino/spiders/valentino_script.py
+++ valentino/spiders/valentino_script.py
@@ -1,11 +1,8 @@
 import scrapy
-# import ValentinoProduct
-#from valentino.items import ValentinoProduct
-# import valentino.items.ValentinoPrice
 from valentino.items import ValentinoProduct
 
 
-class mySpider(scrapy.Spider):
+class MySpider(scrapy.Spider):
     name = 'valent'
     allowed_domains = []
     # start_urls = ['https://www.valentino.com']
@@ -13,32 +10,53 @@ class mySpider(scrapy.Spider):
 
     def __init__(self, url="https://www.valentino.com/", *args, **kwargs):
         self.url = url
-        super(mySpider, self).__init__(mySpider, *args, **kwargs)
+        super(MySpider, self).__init__(MySpider, *args, **kwargs)
 
     def start_requests(self):
         yield scrapy.Request(url=self.url, callback=self.parse)
 
     def parse(self, response):
-        # search_url = "https://www.valentino.com/{}".format('ua/shop/для-женщин/прет-а-порте/рубашки-и-топы#')
-        search_url = "https://www.valentino.com/ua/shop/%D0%B4%D0%BB%D1%8F-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD/%D0%BF%D1%80%D0%B5%D1%82-%D0%B0-%D0%BF%D0%BE%D1%80%D1%82%D0%B5/%D0%BF%D0%BB%D0%B0%D1%82%D1%8C%D1%8F#"
+        search_url = "https://www.valentino.com/{}".format('ua/shop/для-женщин/прет-а-порте/')
         # response
-        yield scrapy.Request(url=search_url, callback=self.result_parse,
-                             dont_filter=True, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/56.0'}, meta={'category': []})
-
-    # def parse.items
-    # yield item
-    # def parse.page
-    # def parse.detail
-
-    def result_parse(self, response):
-        res = response.xpath("/ul/li/a//text()").extract()
-        prod = ValentinoProduct()
-        # prod.name = 'test'
-        # res = response.body
-        print(res)
+        yield scrapy.Request(url=search_url, callback=get_categories,
+                             dont_filter=True,
+                             headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                                                    '(KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'},
+                             meta={'categories': []})
 
 
+def get_categories(response):
+    categories = response.xpath('//ul[@class="level-2"]/li[@id="donna_categories_abbigliamento"]/'
+                                'ul[@class="level-3"]/li/a')
+    for category in categories:
+        url_cat = category.xpath('@href').extract_first()
+        name_cat = category.xpath('span/text()').extract_first()
+        print(name_cat)
+        response.meta['categories'].append(name_cat)
+        # prod = ValentinoProduct()
+        # prod.categories = category_name
+        yield scrapy.Request(
+            url=url_cat,
+            callback=get_items,
+            meta={
+                'categories': response.meta['categories'],
+            }
+        )
 
+
+def get_items(response):
+    # print(response.body)
+    items = response.xpath('//section[@class="search"]/ul[@class="products"]/li[@class="item"]/'
+                           'article[@class="search-item"]/header/a')
+    print(items)
+    for item in items:
+        print(item)
+        url = item.xpath('@href').extract_first()
+        name = item.xpath('title').extract_first()
+        print(name, url)
+        yield scrapy.Request(
+            url=url,
+            callback=get_items)
 
 
 
